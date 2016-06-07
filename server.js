5fd30e4a1641ee641db0bd8a06fa6087660928cc
magicdawn@qq.com <magicdawn@qq.com>
2016-06-08 00:17:42 +0800

    dev: add jsbeautify & eslint

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
+'use strict';
+
 /**
- * module dependencies
+ * module deps
  */
-global.Promise = require('bluebird');
-var co = require('co');
-var _ = require('lodash');
-var http = require('http');
-var fs = Promise.promisifyAll(require('fs'));
-var pathFn = require('path');
-var util = require('util');
-var parse = require('url').parse;
-var zlib = require('zlib');
-var mime = require('mime');
-var bars = require('nodebars');
+
+const co = require('co');
+const _ = require('lodash');
+const http = require('http');
+const fs = Promise.promisifyAll(require('fs'));
+const pathFn = require('path');
+const util = require('util');
+const parse = require('url').parse;
+const zlib = require('zlib');
+const mime = require('mime');
+const bars = require('nodebars');
 
 // patch fs.existAsync
 fs.existsAsync = function(path) {
@@ -42,7 +44,7 @@ function Server(options) {
  *
  * for request event
  */
-Server.prototype.requestListener = co.wrap(function * (req, res) {
+Server.prototype.requestListener = co.wrap(function*(req, res) {
   req.url = decodeURI(req.url);
 
   // /abc => /abc/
@@ -54,7 +56,7 @@ Server.prototype.requestListener = co.wrap(function * (req, res) {
     return;
   }
 
-  var file = yield this.getFileAsync(req, res);
+  const file = yield this.getFileAsync(req, res);
   if (!file) return; // already handled
 
   // if file not exists
@@ -65,25 +67,25 @@ Server.prototype.requestListener = co.wrap(function * (req, res) {
   }
 
   // mime header
-  var ext = pathFn.extname(file).slice(1);
-  res.setHeader("Content-Type", mime.lookup(ext));
-  res.setHeader("Server", 'http-static-server');
+  const ext = pathFn.extname(file).slice(1);
+  res.setHeader('content-type', mime.lookup(ext));
+  res.setHeader('Server', 'http-static-server');
 
   //do cache
-  res.setHeader("Cache-Control", "max-age=" + 365 * 86400 * 1000); //一年ms
-  var now = new Date;
+  res.setHeader('Cache-Control', 'max-age=' + 365 * 86400 * 1000); //一年ms
+  const now = new Date;
   now.setTime(now.getTime() + 365 * 86400 * 1000);
-  res.setHeader("Expires", now.toUTCString());
+  res.setHeader('Expires', now.toUTCString());
 
   try {
-    var fd = yield fs.openAsync(file, "r");
-    var s = yield fs.fstatAsync(fd);
+    const fd = yield fs.openAsync(file, 'r');
+    const s = yield fs.fstatAsync(fd);
 
-    var mtime = s.mtime.toUTCString();
-    res.setHeader("Last-Modified", mtime); //最后修改时间
+    const mtime = s.mtime.toUTCString();
+    res.setHeader('Last-Modified', mtime); //最后修改时间
 
-    if (req.headers["if-modified-since"] &&
-      req.headers["if-modified-since"] === mtime) {
+    if (req.headers['if-modified-since'] &&
+      req.headers['if-modified-since'] === mtime) {
       //未修改
       res.writeHead(304);
       res.end();
@@ -100,12 +102,12 @@ Server.prototype.requestListener = co.wrap(function * (req, res) {
   }
 });
 
-Server.prototype.getFileAsync = co.wrap(function * (req, res) {
-  var url = decodeURI(parse(req.url).pathname); //本来已经decode过了,但是parse里面会把空格变成 %20
+Server.prototype.getFileAsync = co.wrap(function*(req, res) {
+  const url = decodeURI(parse(req.url).pathname); //本来已经decode过了,但是parse里面会把空格变成 %20
 
   // not legal
   if (url.indexOf('../') > -1) {
-    console.error("请求已禁止 : %s", req.url);
+    console.error('请求已禁止 : %s', req.url);
     res.writeHead(400);
     res.end();
     return;
@@ -131,47 +133,45 @@ Server.prototype.getFileAsync = co.wrap(function * (req, res) {
 });
 
 Server.prototype.sendFile = function(req, res, fd) {
-  var stream = fs.createReadStream(null, {
+  let stream = fs.createReadStream(null, {
     fd: fd
   });
 
-  var acc = (req.headers['accept-encoding']);
+  const acc = (req.headers['accept-encoding']);
   if (acc.indexOf('gzip') > -1) {
-    res.setHeader("Content-Encoding", 'gzip');
+    res.setHeader('Content-Encoding', 'gzip');
     stream = stream.pipe(zlib.createGzip());
   } else if (acc.indexOf('deflate') > -1) {
-    res.setHeader("Content-Encoding", 'deflate');
+    res.setHeader('Content-Encoding', 'deflate');
     stream = stream.pipe(zlib.createDeflate());
   }
   stream.pipe(res);
 };
 
-Server.prototype.listDirAsync = co.wrap(function * (req, res) {
-  var self = this;
-  var url = req.url;
-  var dir = this.root + url;
+Server.prototype.listDirAsync = co.wrap(function*(req, res) {
+  const self = this;
+  const url = req.url;
+  const dir = this.root + url;
 
   try {
-    var contents = yield fs.readdirAsync(dir);
+    const contents = yield fs.readdirAsync(dir);
 
-    var files = [];
-    var dirs = [];
+    const files = [];
+    const dirs = [];
 
-    for (var i = 0; i < contents.length; i++) {
-      var c = contents[i];
-      var file = self.root + url + c;
-      var s = yield fs.statAsync(file);
+    for (let i = 0; i < contents.length; i++) {
+      const c = contents[i];
+      const file = self.root + url + c;
+      const s = yield fs.statAsync(file);
 
-      if (s.isDirectory())
-        dirs.push(c);
-      else if (s.isFile())
-        files.push(c);
+      if (s.isDirectory()) dirs.push(c);
+      else if (s.isFile()) files.push(c);
     }
 
     dirs.sort();
     files.sort();
 
-    var html = bars.renderFileSync(__dirname + '/tmpl/index.html', {
+    const html = bars.renderFileSync(__dirname + '/tmpl/index.html', {
       url: url,
       parentdir: pathFn.dirname(url),
       dirs: dirs,
@@ -179,20 +179,20 @@ Server.prototype.listDirAsync = co.wrap(function * (req, res) {
     });
 
     res.writeHead(200, {
-      "Content-Type": 'text/html'
+      'Content-Type': 'text/html'
     });
 
     res.end(html);
   } catch (e) {
-    console.error("列出目录出错 : %s", url);
+    console.error('列出目录出错 : %s', url);
     console.error(e.stack || e);
     res.writeHead(404);
-    res.end(util.format("Can't get %s", url));
+    res.end(util.format('Can\'t get %s', url));
   }
 });
 
 Server.prototype.listen = function() {
-  var self = this;
+  const self = this;
 
   return new Promise(function(resolve, reject) {
     self.server.listen(self.port, function(err) {
@@ -200,7 +200,7 @@ Server.prototype.listen = function() {
         return reject(err);
       }
 
-      var url = 'http://localhost:' + this.address().port;
+      const url = 'http://localhost:' + this.address().port;
       resolve(url);
     });
   });
