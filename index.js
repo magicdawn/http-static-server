c6c1adf5b3867274f29534017f05ac3dff8d7fa9
magicdawn@qq.com <magicdawn@qq.com>
2016-06-08 16:16:32 +0800

    hss: add

--- index.js
+++ index.js
@@ -17,52 +17,58 @@ const modern = require('express-modern');
  * do exports
  */
 
-const app = module.exports = express();
-const pwd = process.cwd();
+module.exports = function setup(root, index) {
+  const app = express();
+  root || process.cwd();
 
-app.use(express.static(pwd, {
-  dotfiles: 'allow',
-  index: false,
-  setHeaders: (res, path, stat) => {
-    if (/^\./.test(basename(path))) {
-      res.type('text');
+  const opts = {
+    dotfiles: 'allow',
+    setHeaders: (res, path, stat) => {
+      if (/^\./.test(basename(path))) {
+        res.type('text');
+      }
+
+      const ext = extname(path).slice(1);
+      if (~['cson', 'less', 'py', 'rb'].indexOf(ext)) {
+        res.type('text');
+      }
+    }
+  };
+
+  if(!index) opts.index = index;
+  app.use(express.static(root, opts));
+
+  // list dir
+  app.use(modern(function*(req, res, next) {
+    if (!_.endsWith(req.path, '/')) return next();
+
+    const rel = req.path.slice(0, -1);
+    if (~rel.indexOf('../')) return res.status(400).send('../ not allowed');
+
+    const dir = root + rel;
+    const contents = yield fs.readdirAsync(dir);
+    const files = [];
+    const dirs = [];
+
+    for (let c of contents) {
+      const file = dir + '/' + c;
+      const s = yield fs.statAsync(file);
+      if (s.isDirectory()) dirs.push(c);
+      else if (s.isFile()) files.push(c);
     }
 
-    const ext = extname(path).slice(1);
-    if(~['cson', 'less', 'py', 'rb'].indexOf(ext)) {
-      res.type('text');
-    }
-  }
-}));
+    dirs.sort();
+    files.sort();
 
-// list dir
-app.use(modern(function*(req, res, next) {
-  if (!_.endsWith(req.path, '/')) return next();
+    const html = swig.renderFile(__dirname + '/tmpl/index.html', {
+      url: req.path,
+      parentdir: dirname(req.path.slice(0, -1)) || '/',
+      dirs: dirs,
+      files: files
+    });
 
-  const rel = req.path.slice(0, -1);
-  if (~rel.indexOf('../')) return res.status(400).send('../ not allowed');
+    res.type('html').send(html);
+  }));
 
-  const dir = pwd + rel;
-  const contents = yield fs.readdirAsync(dir);
-  const files = [];
-  const dirs = [];
-
-  for (let c of contents) {
-    const file = dir + '/' + c;
-    const s = yield fs.statAsync(file);
-    if (s.isDirectory()) dirs.push(c);
-    else if (s.isFile()) files.push(c);
-  }
-
-  dirs.sort();
-  files.sort();
-
-  const html = swig.renderFile(__dirname + '/tmpl/index.html', {
-    url: req.path,
-    parentdir: dirname(req.path.slice(0, -1)) || '/',
-    dirs: dirs,
-    files: files
-  });
-
-  res.type('html').send(html);
-}));
+  return app;
+};
